1586572be7f08e888f3a09de2c5b82733d132e58
Michal Reclaw <micrec90@gmail.com>
2024-05-30 00:08:44 +0200

    Converted to standalone

--- AngularSolution/AngularToDoList/src/main.ts
+++ AngularSolution/AngularToDoList/src/main.ts
@@ -1,8 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
 })
   .catch(err => console.error(err));
