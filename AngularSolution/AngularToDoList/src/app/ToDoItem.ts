f285fae0557e779b670af64bc932e227c9f54cd6
Michal Reclaw <micrec90@gmail.com>
2024-05-30 00:14:22 +0200

    Simple To Do List
    - ToDoItem.ts
    - html with list and add functionality
    - app.component.ts code

---
+++ AngularSolution/AngularToDoList/src/app/ToDoItem.ts
@@ -0,0 +1,4 @@
+export interface ToDoItem {
+  description: string;
+  done: boolean;
+}
--- AngularSolution/AngularToDoList/src/app/app.component.ts
+++ AngularSolution/AngularToDoList/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -5,9 +6,32 @@ import { RouterOutlet } from '@angular/router';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    standalone: true,
-    imports: [RouterOutlet]
+  standalone: true,
+  imports: [RouterOutlet, CommonModule]
 })
 export class AppComponent {
-  title = 'AngularToDoList';
+  title = 'My Angular To Do List';
+  filter: "all" | "active" | "done" = "all";
+  toDoItems = [
+    { description: "wake up", done: true },
+    { description: "eat", done: false },
+    { description: "survive", done: false },
+    { description: "sleep", done: false }
+  ];
+  get items() {
+    if (this.filter === "all") {
+      return this.toDoItems;
+    }
+    return this.toDoItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+  addItem(description: string) {
+    if (!description) return;
+
+    this.toDoItems.unshift({
+      description,
+      done: false
+    });
+  }
 }
